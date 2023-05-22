d6143ef8f71a3ad8cd898931732872fa42219de1
hung202 <artaimer00@gmail.com>
2023-05-22 08:18:20 +0700

    up

--- send_test_email.py
+++ send_test_email.py
@@ -2,16 +2,17 @@
 
 # Imports
 import requests
-
+import os
 def send_simple_message():
-    print("I am sending an email.")
-    return requests.post(
-        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
-        auth=("api", "YOUR_API_KEY"),
-        data={"from": 'hello@example.com',
-            "subject": "Visitor Alert",
-            "html": "<html> Your Raspberry Pi recognizes someone. </html>"})
-                      
+	return requests.post(
+		"https://api.mailgun.net/v3/sandboxa23eea2d073241058ce1703da7ba11a2.mailgun.org/messages",
+		auth=("api", "becd0fee112532e388a257f368b3c942-50f43e91-9541815d"),
+		data={"from": "Me <mailgun@sandboxa23eea2d073241058ce1703da7ba11a2.mailgun.org>",
+			"to": ["artaimer00@gmail.com"],
+			"subject": "test",
+			"text": "xin day met lam r"})
 request = send_simple_message()
 print ('Status: '+format(request.status_code))
 print ('Body:'+ format(request.text))
+os.system("aplay /home/pi/mo.wav")
+
